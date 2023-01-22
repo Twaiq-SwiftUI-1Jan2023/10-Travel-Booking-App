31c635cccaa94d3163a7ccd14536bb54391cbc8d
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2023-01-22 07:34:03 +0300

    Travel Booking App [Data Model]

---
+++ Travel_Booking_App/Travel_Booking_App/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Travel_Booking_App
+//
+//  Created by Raneem AlRashoud on 28/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SignUpView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
